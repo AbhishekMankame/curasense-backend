0737cfc389cdd77d4b0351b5ac817553784c4871
abhishek <abhishekmankame962@gmail.com>
2025-05-14 17:47:37 +0530

    list of disease and sympotms update

--- app/predict.py
+++ app/predict.py
@@ -4,6 +4,15 @@ from typing import List
 
 router = APIRouter()
 
+# Sample rule-based database
+disease_symptom_map = {
+    "Flu" : {"fever", "cough", "sore throat", "fatigue"},
+    "Common Cold" : {"snezzing", "stuffy nose", "cough"},
+    "COVID-19" : {"fever", "cough", "loss of taste", "shortness of breath"},
+    "Malaria" : {"fever", "chills", "sweating", "headache"},
+    "Dengue" : {"fever", "rash", "joint pain", "nausea"}
+}
+
 # Define a request model for disease prediction
 class DiseasePredictionRequest(BaseModel):
     symptoms: List[str] # List of symptoms as strings
