58253f189a13117a28b4f868cd9b99cbc2a8da3d
abhishek <abhishekmankame962@gmail.com>
2025-05-15 23:58:53 +0530

    code update

--- main.py
+++ main.py
@@ -1,20 +1,10 @@
 from fastapi import FastAPI
-from dotenv import load_dotenv
-from app.predict import predict_disease, DiseasePredictionRequest # Import the whole predict module
-import os
+from app.routes import predict # Import the router module
 
-# Load environment variables
-load_dotenv()
-
-# Initialize the FASTAPI app
 app = FastAPI()
 
-# Root endpoint
 @app.get("/")
 def read_root():
-    return {"message": "Hello, Welcome to CuraSense!"}
+    return {"message" : "Hello, welcome to CuraSense"}
 
-# Include the predict router
-@app.post("/predict_disease")
-def predict_symptoms(request: DiseasePredictionRequest):
-    return predict_disease(request)
+app.include_router(predict.router)
