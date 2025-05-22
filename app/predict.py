2c5f197bb042f445663105a31c08b672098c2d03
abhishek <abhishekmankame962@gmail.com>
2025-05-22 23:44:22 +0530

    diease and symptoms update

--- app/predict.py
+++ app/predict.py
@@ -4,4 +4,8 @@ from typing import List
 SYMPTOM_DISEASE_MAP = {
     "fever": ["Flu", "Common Cold", "COVID-19"],
     "cough": ["Common Cold", "Flu", "Bronchitis"],
+    "headache": ["Migraine", "Flu", "Tension Headache"],
+    "fatigue": ["Anemia", "Flu", "COVID-19"],
+    "rash": ["Allergy", "Chickenpox"],
+    # Add more mappings as needed
 }
